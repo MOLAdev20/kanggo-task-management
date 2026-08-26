e709afa26d2a3f1beffb5deec7bf0e249bc4bb29
Sabilul Hikam <billhikam2003@gmail.com>
2026-08-26 14:56:40 +0700

    feat: implement user registration with password hashing

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,17 +1,49 @@
 import { type Request, type Response } from "express";
+import { prisma } from "../lib/prisma";
+import bcrypt from "bcrypt";
 
 const endpoint = {
-  register: (req: Request, res: Response) => {
-    const { name, email, password } = req.body;
+  register: async (req: Request, res: Response) => {
+    try {
+      const { name, email, password } = req.body;
 
-    res.json({
-      message: "Selamat datang",
-      body: {
-        name,
-        email,
-        password,
-      },
-    });
+      if (!name || !email || !password) {
+        return res.status(400).json({
+          message: "missing-required-fields",
+        });
+      }
+
+      // Cek is requested email has been registered?
+      const registeredEmail = await prisma.user.count({
+        where: { email },
+      });
+
+      if (registeredEmail) {
+        return res.status(409).json({
+          message: "email-already-registered",
+        });
+      }
+
+      // store to db, @User
+      const hashPassword = await bcrypt.hash(password, 10);
+      const newUser = await prisma.user.create({
+        data: {
+          name,
+          email,
+          password: hashPassword,
+        },
+      });
+
+      res.json({
+        message: "user-created",
+        newUser,
+      });
+    } catch (err: any) {
+      res.status(500).json({
+        message: "internal-server-error",
+        detail: err,
+      });
+    }
   },
 };
 
