c04e9090a85d653c3a70ba4106ac562e0e29d723
Sabilul Hikam <billhikam2003@gmail.com>
2026-08-26 13:40:23 +0700

    initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import { type Request, type Response } from "express";
+import app from "./app.ts";
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({
+    message: "Hello World",
+  });
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, (err) => {
+  if (err) console.error(err);
+
+  console.log(`✅ Server is running on port ${port}`);
+});
